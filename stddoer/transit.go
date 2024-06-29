0d27720826f61ea6a6e69580b1274fbe40fca1b3
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2024-06-29 14:34:26 +0300

    fail if flow id not set on transit

--- stddoer/transit.go
+++ stddoer/transit.go
@@ -1,12 +1,20 @@
 package stddoer
 
-import "github.com/makasim/flowstate"
+import (
+	"fmt"
+
+	"github.com/makasim/flowstate"
+)
 
 func Transit() flowstate.Doer {
 	return flowstate.DoerFunc(func(cmd0 flowstate.Command) error {
 		cmd, ok := cmd0.(*flowstate.TransitCommand)
 		if !ok {
 			return flowstate.ErrCommandNotSupported
+		}
+
+		if cmd.FlowID == "" {
+			return fmt.Errorf("flow id empty")
 		}
 
 		cmd.StateCtx.Transitions = append(cmd.StateCtx.Transitions, cmd.StateCtx.Current.Transition)
