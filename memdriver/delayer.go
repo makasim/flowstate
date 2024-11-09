435dc50b732d3daab4c0b35e151ac496f95aa503
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2024-11-09 12:18:48 +0200

    memdriver wait for delayer workers stop

--- memdriver/delayer.go
+++ memdriver/delayer.go
@@ -3,7 +3,9 @@ package memdriver
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/makasim/flowstate"
@@ -13,12 +15,13 @@ var _ flowstate.Doer = &Delayer{}
 
 type Delayer struct {
 	e      *flowstate.Engine
-	doneCh chan struct{}
+	stopCh chan struct{}
+	wg     sync.WaitGroup
 }
 
 func NewDelayer() *Delayer {
 	return &Delayer{
-		doneCh: make(chan struct{}),
+		stopCh: make(chan struct{}),
 	}
 }
 
@@ -33,7 +36,10 @@ func (d *Delayer) Do(cmd0 flowstate.Command) error {
 	}
 
 	// todo: replace naive implementation with real one
+	d.wg.Add(1)
 	go func() {
+		defer d.wg.Done()
+
 		t := time.NewTimer(cmd.Duration)
 		defer t.Stop()
 
@@ -55,7 +61,7 @@ func (d *Delayer) Do(cmd0 flowstate.Command) error {
 			if err := d.e.Execute(cmd.DelayStateCtx); err != nil {
 				log.Printf(`ERROR: memdriver: delayer: engine: execute: %s`, err)
 			}
-		case <-d.doneCh:
+		case <-d.stopCh:
 			log.Printf(`ERROR: memdriver: delayer: state delay was terminated`)
 			return
 		}
@@ -66,10 +72,30 @@ func (d *Delayer) Do(cmd0 flowstate.Command) error {
 
 func (d *Delayer) Init(e *flowstate.Engine) error {
 	d.e = e
+	d.wg.Add(1)
 	return nil
 }
 
-func (d *Delayer) Shutdown(_ context.Context) error {
-	close(d.doneCh)
-	return nil
+func (d *Delayer) Shutdown(ctx context.Context) error {
+	select {
+	case <-d.stopCh:
+		return fmt.Errorf(`already stopped`)
+	default:
+	}
+
+	close(d.stopCh)
+	d.wg.Done()
+
+	stoppedCh := make(chan struct{})
+	go func() {
+		d.wg.Wait()
+		close(stoppedCh)
+	}()
+
+	select {
+	case <-stoppedCh:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
 }
