6d570a2e969cb2084a2bacf76bfbde9385a42fa9
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2024-11-13 21:06:11 +0200

    slow improvements

--- log.go
+++ log.go
@@ -27,6 +27,10 @@ func logExecute(stateCtx *StateCtx, l *slog.Logger) {
 		args = append(args, "recovered", currTs.Annotations[RecoveryAttemptAnnotation])
 	}
 
+	if len(stateCtx.Current.Labels) > 0 {
+		args = append(args, "labels", stateCtx.Current.Labels)
+	}
+
 	l.Info("engine: execute", args...)
 }
 
@@ -39,22 +43,73 @@ func logDo(cmd0 Command, l *slog.Logger) {
 	case *CommitStateCtxCommand:
 		args = append(args, "cmd", "commit_state_ctx", "id", cmd.StateCtx.Current.ID, "rev", cmd.StateCtx.Current.Rev)
 	case *TransitCommand:
-		args = append(args, "cmd", "transit", "id", cmd.StateCtx.Current.ID, "rev", cmd.StateCtx.Current.Rev, "to", cmd.FlowID)
+		args = append(args,
+			"cmd", "transit",
+			"id", cmd.StateCtx.Current.ID,
+			"rev", cmd.StateCtx.Current.Rev,
+			"to", cmd.FlowID,
+		)
+		if len(cmd.StateCtx.Current.Labels) > 0 {
+			args = append(args, "labels", cmd.StateCtx.Current.Labels)
+		}
 	case *PauseCommand:
-		args = append(args, "cmd", "pause", "id", cmd.StateCtx.Current.ID, "rev", cmd.StateCtx.Current.Rev)
+		args = append(args,
+			"cmd", "pause",
+			"id", cmd.StateCtx.Current.ID,
+			"rev", cmd.StateCtx.Current.Rev,
+		)
 		if cmd.FlowID != `` {
 			args = append(args, "to", cmd.FlowID)
 		}
+		if len(cmd.StateCtx.Current.Labels) > 0 {
+			args = append(args, "labels", cmd.StateCtx.Current.Labels)
+		}
 	case *ResumeCommand:
-		args = append(args, "cmd", "resume", "id", cmd.StateCtx.Current.ID, "rev", cmd.StateCtx.Current.Rev)
+		args = append(args,
+			"cmd", "resume",
+			"id", cmd.StateCtx.Current.ID,
+			"rev", cmd.StateCtx.Current.Rev,
+		)
+		if len(cmd.StateCtx.Current.Labels) > 0 {
+			args = append(args, "labels", cmd.StateCtx.Current.Labels)
+		}
 	case *EndCommand:
-		args = append(args, "cmd", "end", "id", cmd.StateCtx.Current.ID, "rev", cmd.StateCtx.Current.Rev)
+		args = append(args,
+			"cmd", "end",
+			"id", cmd.StateCtx.Current.ID,
+			"rev", cmd.StateCtx.Current.Rev,
+		)
+		if len(cmd.StateCtx.Current.Labels) > 0 {
+			args = append(args, "labels", cmd.StateCtx.Current.Labels)
+		}
 	case *DelayCommand:
-		args = append(args, "cmd", "delay", "id", cmd.StateCtx.Current.ID, "rev", cmd.StateCtx.Current.Rev, "dur", cmd.Duration)
+		args = append(args,
+			"cmd", "delay",
+			"id", cmd.StateCtx.Current.ID,
+			"rev", cmd.StateCtx.Current.Rev,
+			"dur", cmd.Duration,
+		)
+		if len(cmd.StateCtx.Current.Labels) > 0 {
+			args = append(args, "labels", cmd.StateCtx.Current.Labels)
+		}
 	case *ExecuteCommand:
-		args = append(args, "cmd", "execute", "id", cmd.StateCtx.Current.ID, "rev", cmd.StateCtx.Current.Rev)
+		args = append(args,
+			"cmd", "execute",
+			"id", cmd.StateCtx.Current.ID,
+			"rev", cmd.StateCtx.Current.Rev,
+		)
+		if len(cmd.StateCtx.Current.Labels) > 0 {
+			args = append(args, "labels", cmd.StateCtx.Current.Labels)
+		}
 	case *NoopCommand:
-		args = append(args, "cmd", "noop", "id", cmd.StateCtx.Current.ID, "rev", cmd.StateCtx.Current.Rev)
+		args = append(args,
+			"cmd", "noop",
+			"id", cmd.StateCtx.Current.ID,
+			"rev", cmd.StateCtx.Current.Rev,
+		)
+		if len(cmd.StateCtx.Current.Labels) > 0 {
+			args = append(args, "labels", cmd.StateCtx.Current.Labels)
+		}
 	case *ReferenceDataCommand:
 		args = append(args,
 			"cmd", "reference_data",
@@ -85,6 +140,18 @@ func logDo(cmd0 Command, l *slog.Logger) {
 		args = append(args, "cmd", "get_flow", "flow_id", cmd.StateCtx.Current.Transition.ToID)
 	case *WatchCommand:
 		args = append(args, "cmd", "watch")
+		if cmd.SinceLatest == true {
+			args = append(args, "since_latest", "true")
+		}
+		if cmd.SinceRev > 0 {
+			args = append(args, "since_rev", cmd.SinceRev)
+		}
+		if !cmd.SinceTime.IsZero() {
+			args = append(args, "since_time", cmd.SinceTime)
+		}
+		if len(cmd.Labels) > 0 {
+			args = append(args, "labels", cmd.Labels)
+		}
 	default:
 		args = append(args, "cmd", fmt.Sprintf("%T", cmd))
 	}
