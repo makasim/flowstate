1755d48820e3fe683de765110cf88b5352414287
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2025-07-18 21:35:39 +0300

    upd examples

--- examples/durable_execute.go
+++ examples/durable_execute.go
@@ -16,7 +16,7 @@ func main() {
 	err := d.SetFlow(`example`, flowstate.FlowFunc(func(stateCtx *flowstate.StateCtx, _ flowstate.Engine) (flowstate.Command, error) {
 		slog.Default().Info(fmt.Sprintf("executing state: %s", stateCtx.Current.ID))
 
-		// put your business logic here
+		// Put your business logic here
 
 		// Tell then engine that the state is executed successfully
 		return flowstate.Commit(flowstate.End(stateCtx)), nil
@@ -30,11 +30,12 @@ func main() {
 	}
 
 	// After the state is commited it is guaranteed that the state will be executed
-	// at least flowstate.DefaultMaxRecoveryAttempts
+	// at least flowstate.DefaultMaxRecoveryAttempts attempts
 	err = e.Do(flowstate.Commit(flowstate.Transit(stateCtx, `example`)))
 	handleError(err)
 
 	// Execute the state synchronously
+	// The engine will call the example flow.
 	err = e.Execute(stateCtx)
 	handleError(err)
 }
